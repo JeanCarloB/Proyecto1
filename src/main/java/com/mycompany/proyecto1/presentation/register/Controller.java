1983ce439a5ffe6102721d3e33970173f4faf645
BrenesRi <ribrenes10@gmail.com>
2023-04-14 11:22:40 -0600

    Metodos Controller

--- src/main/java/com/mycompany/proyecto1/presentation/register/Controller.java
+++ src/main/java/com/mycompany/proyecto1/presentation/register/Controller.java
@@ -1,21 +1,22 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.proyecto1.presentation.register;
 
-/**
- *
- * @author Kevin
- */
 
+import com.mycompany.proyecto1.logic.Service;
+import com.mycompany.proyecto1.logic.Usuario;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
+/**
+ *
+ * @author ribre
+ */
 @WebServlet(name = "RegisterController", urlPatterns = {"/presentation/register/show"})
 public class Controller extends HttpServlet {
 
@@ -30,16 +31,56 @@ public class Controller extends HttpServlet {
             case "/presentation/register/show":
                 viewUrl=this.show(request);
                 break;              
-            
+     
         }
         request.getRequestDispatcher(viewUrl).forward( request, response); 
   }
-  
-    public String show(HttpServletRequest request){
-            return this.showAction(request);
-        }
 
-    public String showAction(HttpServletRequest request){
-        return "/presentation/register/View.jsp"; 
-    }   
+    private String show(HttpServletRequest request) {
+        return this.showAction(request);
+    }
+
+    private String showAction(HttpServletRequest request) {
+        return "/presentation/register/View.jsp";
+    }
+    
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
 }
+   
