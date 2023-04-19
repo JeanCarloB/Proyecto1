1a2dd5dd47a075fff19337e738abdb672c7665d2
JEAN CARLO BERMUDEZ MORA <76853715+JeanCarloB@users.noreply.github.com>
2023-04-19 05:51:15 -0600

    Cambios en el clienteDAO que afectaban la recuperacion de los datos

--- src/main/java/com/mycompany/proyecto1/data/ClienteDao.java
+++ src/main/java/com/mycompany/proyecto1/data/ClienteDao.java
@@ -1,7 +1,7 @@
-
 package com.mycompany.proyecto1.data;
 
 import com.mycompany.proyecto1.logic.Cliente;
+import com.mycompany.proyecto1.logic.Usuario;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -13,31 +13,41 @@ import java.util.List;
  * @author ribre
  */
 public class ClienteDao {
+
     RelDatabase db;
 
-    public ClienteDao(RelDatabase db){
-        this.db= db;
-    }    
-    
+    public ClienteDao(RelDatabase db) {
+        this.db = db;
+    }
+
     public Cliente read(String cedula) throws Exception {
-        String sql = "select " +
-                "* " +
-                "from Cliente e inner join Usuario u on e.usuario=u.cedula " +
-                "where e.cedula=?";
-        PreparedStatement stm = db.prepareStatement(sql);
-        stm.setString(1, cedula);
-        ResultSet rs = db.executeQuery(stm);
-        UsuarioDao usuarioDao = new UsuarioDao(db);
-        Cliente c;
+        String sql = "SELECT c.*, u.clave, u.tipo "
+                + "FROM Cliente c "
+                + "JOIN Usuario u ON c.usuario = u.cedula "
+                + "WHERE c.cedula = ?";
+        PreparedStatement statement = db.prepareStatement(sql);
+        statement.setString(1, cedula);
+        ResultSet rs = statement.executeQuery();
         if (rs.next()) {
-            c= from(rs, "e");
-            c.setUsuario(usuarioDao.from(rs,"u"));
-            return c;
+            Cliente cliente = new Cliente();
+            cliente.setCedula(rs.getString("cedula"));
+            cliente.setNombre(rs.getString("nombre"));
+            cliente.setTelefono(rs.getString("telefono"));
+            cliente.setCorreo(rs.getString("correo"));
+            cliente.setTarjeta(rs.getString("tarjeta"));
+
+            Usuario usuario = new Usuario();
+            usuario.setCedula(rs.getString("usuario"));
+            usuario.setClave(rs.getString("clave"));
+            usuario.setTipo(rs.getInt("tipo"));
+
+            cliente.setUsuario(usuario);
+            return cliente;
         } else {
-            throw new Exception("Empleado no Existe");
+            return null;
         }
     }
-    
+
     public Cliente from(ResultSet rs, String alias) {
         try {
             Cliente e = new Cliente();
@@ -47,16 +57,16 @@ public class ClienteDao {
         } catch (SQLException ex) {
             return null;
         }
-    }    
+    }
 
     public void update(Cliente e) throws Exception {
-        String sql = "update " +
-                "Cliente " +
-                "set nombre=? , " +
-                " telefono=? , " +
-                " correo=? , " +
-                " tarjeta=? " +
-                "where cedula=?";
+        String sql = "update "
+                + "Cliente "
+                + "set nombre=? , "
+                + " telefono=? , "
+                + " correo=? , "
+                + " tarjeta=? "
+                + "where cedula=?";
         PreparedStatement stm = db.prepareStatement(sql);
         stm.setString(1, e.getNombre());
         stm.setString(2, e.getTelefono());
@@ -67,13 +77,14 @@ public class ClienteDao {
         if (count == 0) {
             throw new Exception("Cliente no existe");
         }
-        
+
     }
+
     public void create(Cliente e) throws Exception {
-        String sql = "insert into " +
-                "Cliente " +
-                "(cedula, nombre, telefono, correo, tarjeta, usuario) " +
-                "values(?,?,?,?,?,?)";
+        String sql = "insert into "
+                + "Cliente "
+                + "(cedula, nombre, telefono, correo, tarjeta, usuario) "
+                + "values(?,?,?,?,?,?)";
         PreparedStatement stm = db.prepareStatement(sql);
         stm.setString(1, e.getCedula());
         stm.setString(2, e.getNombre());
@@ -99,4 +110,4 @@ public class ClienteDao {
         }
         return resultado;
     }
-   }
+}
